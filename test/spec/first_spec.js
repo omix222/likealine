5b4af6ef4caa0a3c707b8a7322e88921dc16c16e
Hiromi Takahashi <hiromix222@gmail.com>
2017-07-21 22:13:04 +0900

    #2 first test pass

--- test/spec/first_spec.js
+++ test/spec/first_spec.js
@@ -1,14 +1,92 @@
+// 実行方法
+// jasmine-node .
+
 var frisby = require("frisby");
 
-frisby.create("SAMPLE TEST").get(
+frisby.create("messages get each  TEST").get(
 	// リクエストするAPIパス
-	"http://localhost:3000/messages?groupId=group2"
-).expectJSON(0,
+	"http://localhost:3000/messages/1"
+).expectJSON(
 	// 期待するレスポンスボディ
 	{
-		"messageID": "messageID:3",
+		"messageId": 1,
 		"type": "text",
-		"messageDetail": "Another Group!",
-		"fromUserId": "Ozaki",
-		"groupId": "group2"
-	}).toss();
+		"messageDetail": "Hello",
+		"fromUserId": "u001",
+		"groupId": "g001",
+		"postDate": "2017-07-15T00:26:46.622Z"
+	}
+	).toss();
+
+// frisby.create("messages get each  TEST").get(
+// 	// リクエストするAPIパス
+// 	"http://localhost:3000/messages"
+// ).expectJSON(
+// 	// 期待するレスポンスボディ
+// 	{
+// 		"messageId": 10,
+// 		"type": "test",
+// 		"messageDetail": "first posted",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:16:37.779Z"
+// 	},
+// 	{
+// 		"messageId": 9,
+// 		"type": "test",
+// 		"messageDetail": "first posted",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:16:06.699Z"
+// 	},
+// 	{
+// 		"messageId": 8,
+// 		"type": "test",
+// 		"messageDetail": "first posted",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:06:59.165Z"
+// 	},
+// 	{
+// 		"messageId": 7,
+// 		"type": "test",
+// 		"messageDetail": "first posted",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:06:21.107Z"
+// 	},
+// 	{
+// 		"messageId": 6,
+// 		"type": "test",
+// 		"messageDetail": "first posted",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:04:42.942Z"
+// 	},
+// 	{
+// 		"messageId": 5,
+// 		"type": "text",
+// 		"messageDetail": "mongo sample",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T01:02:09.766Z"
+// 	},
+// 	{
+// 		"messageId": 2,
+// 		"type": "text",
+// 		"messageDetail": "Hello again!",
+// 		"fromUserId": "u002",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T00:26:46.782Z"
+// 	},
+// 	{
+// 		"messageId": 1,
+// 		"type": "text",
+// 		"messageDetail": "Hello",
+// 		"fromUserId": "u001",
+// 		"groupId": "g001",
+// 		"postDate": "2017-07-15T00:26:46.622Z"
+// 	}
+
+
+// 	).toss();
